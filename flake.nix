3ee31fc7dd370fb68a9da52e271ef78a97de5012
WuX <wux@thenest.dev>
2025-01-01 21:41:03 +0200

    add nix flake for devshell and package

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils, ... } @ inputs: flake-utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+            inherit (pkgs) lib mkShell buildGoModule;
+        in {
+            devShells.default = mkShell {
+                shellHook = "export DEVSHELL_PS1_PREFIX='mkimg'";
+                nativeBuildInputs = with pkgs; [
+                    go
+                ];
+            };
+
+            defaultPackage = buildGoModule rec {
+                name = "mkimg";
+
+                src = self;
+
+                vendorHash = "sha256-7AocOQd9Jwz7gFQZG07YYXygBDgKIbKyDU0wf4eHc3s=";
+
+                meta = {
+                    description = "mkimg is a tiny tool to simplify the process of creating partitioned disk images";
+                    homepage = https://git.thenest.dev/wux/mkimg;
+                    maintainers = with lib.maintainers; [ wux ];
+                };
+            };
+        }
+    );
+}
