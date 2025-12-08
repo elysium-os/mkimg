494079b155c0b7c7f0a0e79fe558c87453baccbf
WuX <wux@thenest.dev>
2025-12-08 21:05:57 +0200

    chore: update nix flake

--- flake.nix
+++ flake.nix
@@ -4,31 +4,40 @@
         flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
-        let
-            pkgs = import nixpkgs { inherit system; };
-            inherit (pkgs) lib mkShell buildGoModule;
-        in {
-            devShells.default = mkShell {
-                shellHook = "export NIX_SHELL_NAME='mkimg'";
-                nativeBuildInputs = with pkgs; [
-                    go
-                ];
-            };
-
-            defaultPackage = buildGoModule rec {
-                name = "mkimg";
-
-                src = self;
-
-                vendorHash = "sha256-XcQHwYc3pfAft1hCYywCtDmhMSJM+FDdGOrwZUme8As=";
-
-                meta = {
-                    description = "mkimg is a tiny tool to simplify the process of creating partitioned disk images";
-                    homepage = "https://github.com/elysium-os/mkimg";
-                    maintainers = with lib.maintainers; [ wux ];
+    outputs =
+        {
+            self,
+            nixpkgs,
+            flake-utils,
+            ...
+        }:
+        flake-utils.lib.eachDefaultSystem (
+            system:
+            let
+                pkgs = import nixpkgs { inherit system; };
+                inherit (pkgs) lib mkShell buildGoModule;
+            in
+            {
+                devShells.default = mkShell {
+                    shellHook = "export NIX_SHELL_NAME='mkimg'";
+                    nativeBuildInputs = with pkgs; [
+                        go
+                    ];
                 };
-            };
-        }
-    );
+
+                defaultPackage = buildGoModule rec {
+                    name = "mkimg";
+
+                    src = self;
+
+                    vendorHash = "sha256-7jxXSbJKA/gqHMTxbTKVdZMEOYx6fyNzcL/XOpcpBMc=";
+
+                    meta = {
+                        description = "mkimg is a tiny tool to simplify the process of creating partitioned disk images";
+                        homepage = "https://github.com/elysium-os/mkimg";
+                        maintainers = with lib.maintainers; [ wux ];
+                    };
+                };
+            }
+        );
 }
